21e3f913f2f9c8c094f3ed00102e3666cabbf276
IVANFPARDO <ivanfpardo@gmail.com>
2025-05-13 21:39:51 +0200

    Refactorización "Move Method"
    El método getFrecuentRenterPoints de la clase Customer es más apropiado que esté en la clase Rental porque hace referencia a propiedades suyas. (Code Smell: Shotgun surgery).
    
    Se realiza refactorización: Move Method de getFrecuentRenterPoints renombrándola como getFrecuentPoints y con modificador de acceso public.

--- src/ubu/gii/dass/refactoring/Customer.java
+++ src/ubu/gii/dass/refactoring/Customer.java
@@ -41,7 +41,7 @@ public class Customer {
 			// determine amounts for each line
 			double thisAmount = each.getCharge();
 			
-			frequentRenterPoints += getFrequentRenterPoints(each);
+			frequentRenterPoints += each.getFrecuentPoints();
 			// show figures for this rental
 			result += "\t" + each.getMovie().getTitle() + "\t"
 					+ String.valueOf(thisAmount) + "\n";
@@ -52,15 +52,5 @@ public class Customer {
 		result += "You earned " + String.valueOf(frequentRenterPoints)
 				+ " frequent renter points";
 		return result;
-	}
-
-	private int getFrequentRenterPoints(Rental each) {
-		int frequentRenterPoints=1;
-				
-		// add bonus for a two day new release rental
-		if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
-				&& each.getDaysRented() > 1)
-			frequentRenterPoints++;
-		return frequentRenterPoints;
 	}
 }
