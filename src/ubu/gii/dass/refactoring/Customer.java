dd2358bc92897a7045482e4c84905272b0ab47c3
IVANFPARDO <ivanfpardo@gmail.com>
2025-05-13 21:34:36 +0200

    Refactorización "Extract Method"
    El método statement() de la clase Customer es largo y contiene lógica compleja para el cálculo de los montos y puntos frecuentes. (Bad Smell: Long Method)
    Se realiza una Refactorización "Extract Method" del cálculo de getFrecuentRenterPoints. Para poder ajustar el código se realizo también ajuste de los parámetros

--- src/ubu/gii/dass/refactoring/Customer.java
+++ src/ubu/gii/dass/refactoring/Customer.java
@@ -41,12 +41,7 @@ public class Customer {
 			// determine amounts for each line
 			double thisAmount = each.getCharge();
 			
-			// add frequent renter points
-			frequentRenterPoints++;
-			// add bonus for a two day new release rental
-			if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
-					&& each.getDaysRented() > 1)
-				frequentRenterPoints++;
+			frequentRenterPoints += getFrequentRenterPoints(each);
 			// show figures for this rental
 			result += "\t" + each.getMovie().getTitle() + "\t"
 					+ String.valueOf(thisAmount) + "\n";
@@ -57,5 +52,15 @@ public class Customer {
 		result += "You earned " + String.valueOf(frequentRenterPoints)
 				+ " frequent renter points";
 		return result;
+	}
+
+	private int getFrequentRenterPoints(Rental each) {
+		int frequentRenterPoints=1;
+				
+		// add bonus for a two day new release rental
+		if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
+				&& each.getDaysRented() > 1)
+			frequentRenterPoints++;
+		return frequentRenterPoints;
 	}
 }
